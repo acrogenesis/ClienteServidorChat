5e5c05fa665ca2726082f34c0d6efb9dff7d2a42
Adrian Rangel <adrian.rangel@gmail.com>
2013-11-28 15:16:45 -0600

    println en sendMessage

--- Servidor.java
+++ Servidor.java
@@ -121,7 +121,7 @@ public static void sendPrivateMessage(DatagramSocket yo, String aMandar, String 
         paquete = new DatagramPacket(buffer,buffer.length, userARecibir.getDir(), userARecibir.getPuerto());
       try{
         yo.send(paquete);
-		System.out.println("sending message");
+        System.out.println("sending message: " + aMandar);
       }catch(IOException e){
         System.out.println(e.getMessage());
         System.exit(1);
