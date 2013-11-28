e0f0ea10fcfef41e2fbccd8cca407525832619d3
Montse Lozano Dieck <montse_967@hotmail.com>
2013-11-27 19:45:17 -0600

    cliente con nickname

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,6 @@
 import javax.swing.*;  
 import java.awt.*;  
+import javax.swing.JOptionPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -16,19 +17,18 @@ public class Cliente {
 		JFrame ventana_chat=null;
 		
 		//conectar sesion 
-		JTextField txt_nick = null;
-		JButton btn_conectar = null;
+		static JButton btn_conectar = null;
 		JPanel contenedor_btnConnect = null;
-		String nickname = null;
+		static String nickname = null;
 			
 		//enviar mensaje
-		JButton btn_enviar = null;
-		JTextField txt_mensaje = null;
-		JTextField txt_ip_dm = null;
+		static JButton btn_enviar = null;
+		static JTextField txt_mensaje = null;
+		static JTextField txt_ip_dm = null;
 		JPanel contenedor_btntxt= null;
 		
 		//Area de Mensajes del chat 
-		JTextArea area_chat = null;
+		static JTextArea area_chat = null;
 		JScrollPane scroll = null;
 		JPanel contenedor_areachat = null;
 		
@@ -55,10 +55,8 @@ public class Cliente {
 				
 				//conectar 
 				btn_conectar = new JButton("Conectar");
-				txt_nick = new JTextField();
 				contenedor_btnConnect = new JPanel();
-				contenedor_btnConnect.setLayout(new GridLayout(1,2));
-				contenedor_btnConnect.add(txt_nick);
+				contenedor_btnConnect.setLayout(new GridLayout(1,1));
 				contenedor_btnConnect.add(btn_conectar);
 					
 				
@@ -91,10 +89,27 @@ public class Cliente {
 				ventana_chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				
 			}
+			
+	public static void conectar(){
+		
+		btn_conectar.addActionListener(new ActionListener()
+		{
+					
+					public void actionPerformed(ActionEvent e)
+					{
+						String nombre = JOptionPane.showInputDialog("Nickname: ");
+						nickname = nombre;
+						System.out.println(nombre);
+					}
+					
+		});
+	}
 		
 	
 	public static void main(String[] args) {
 		
 		new Cliente();
+		conectar();
+		
 	}
 }
