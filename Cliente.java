ba8f174ff65fd670d6e06df721129c143cf02355
Montse Lozano Dieck <montse_967@hotmail.com>
2013-11-27 19:18:51 -0600

    nuevo cliente

---
+++ Cliente.java
@@ -0,0 +1,23 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.util.Scanner;
+import java.net.*;
+import java.io.IOException;
+
+public class Cliente {
+	
+	public static void main(String[] args) {
+		
+	}
+}
